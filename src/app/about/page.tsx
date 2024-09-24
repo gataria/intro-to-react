2c9efb32d0b56c6fc86d253a827b2e479065296c
Rodrigo Davila Castillo <rodrigo.davilacastillo@tamucc.edu>
2024-09-24 00:53:16 -0500

    Added styling using CSS!

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,13 @@
+import styles from './styles.module.css';
+
 export default function AboutPage() {
     return (
         <>
             <h1 className="text-3xl font-bold">About</h1>
             <p>Hello there.<br />How do you do?</p>
+            <div className={styles.avatar}>
+                I DON'T HAVE AN IMAGE BUT THIS WILL DO
+            </div>
         </>
     );
 }
