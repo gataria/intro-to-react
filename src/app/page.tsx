faf902fabe2c26dc3c473081eec167fc903e9a4c
Rodrigo Davila Castillo <rodrigo.davilacastillo@tamucc.edu>
2024-09-24 01:35:04 -0500

    Rendering lists, using TypeScript interface for list items' type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+interface listType {
+  title: string,
+  id: number,
+}
+
 function MyButton() {
   return (
     <button className="bg-fuchsia-700 p-1 rounded">
@@ -7,12 +12,32 @@ function MyButton() {
 }
 
 export default function Home() {
+  const list : listType[] = [
+    { title: "Bread", id: 1 },
+    { title: "Milk", id: 2 },
+    { title: "Eggs", id: 3 },
+  ];
+  
+  const listItems = list.map(listItem => 
+    <li
+      key={listItem.id}
+      style={{
+        color: ( listItem.id === 1 ) ? 'red' : 'white',
+      }}
+    >
+      {listItem.title}
+    </li>
+  );
+
   return (
     <div>
       <h1 className="text-3xl font-bold">
         Welcome to my app
       </h1>
       <MyButton />
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }
