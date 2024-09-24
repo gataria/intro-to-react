0aa2d34df02eea274a31de85bd3dfd0a91ea8dcb
Rodrigo Davila Castillo <rodrigo.davilacastillo@tamucc.edu>
2024-09-24 00:48:30 -0500

    Removed underline on h1s. It's tacky.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
         <>
-            <h1>About</h1>
+            <h1 className="text-3xl font-bold">About</h1>
             <p>Hello there.<br />How do you do?</p>
         </>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ function MyButton() {
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold">
         Welcome to my app
       </h1>
       <MyButton />
